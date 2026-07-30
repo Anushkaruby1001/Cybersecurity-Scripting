983f2223e4b70d1dd33bb1674d10f56f5f8929b5
Student apple <anushkanagvekar10@gmail.com>
2026-07-30 19:40:47 +0530

    log analyzation scripting

---
+++ temp.py
@@ -0,0 +1,12 @@
+import re
+file = open("security.log", "r")
+content = file.read()
+
+ips = re.findall(r"\d+\.\d+\.\d+\.\d+", content)
+emails = re.findall(r"\w+@\w+\.\w+", content)
+print("IP addresses found:")
+print(ips)
+
+print("Email(s) Found:")
+print(emails)
+file.close()
