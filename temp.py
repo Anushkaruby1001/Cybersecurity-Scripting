1118c657b31c3e6ff00b3b8cf5c34f9bf8df905d
Student apple <anushkanagvekar10@gmail.com>
2026-07-30 19:45:03 +0530

    mistake resolved

--- temp.py
+++ temp.py
@@ -11,18 +11,3 @@ print("Email(s) Found:")
 print(emails)
 file.close()
 
-import re
-
-file = open("security.log", "r")
-content = file.read()
-
-ips = re.findall(r"\d+\.\d+\.\d+\.\d+", content)
-emails = re.findall(r"\w+@\w+\.\w+", content)
-
-print("IP addresses found:")
-print(ips)
-
-print("Email(s) Found:")
-print(emails)
-
-file.close()
